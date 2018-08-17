64ab3a32a3ba1b440ef3d71515b91cbfc18f7b60
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-08-16 21:10:16 -0300

    Adição de dropdowns (M13a04)

--- src/app/lancamentos-cadastro/lancamentos-cadastro.component.ts
+++ src/app/lancamentos-cadastro/lancamentos-cadastro.component.ts
@@ -11,4 +11,16 @@ export class LancamentosCadastroComponent {
     { label: 'Despesa', value: 'DESPESA' },
     { label: 'Receita', value: 'RECEITA' }
   ];
+
+  categorias = [
+    { label: 'Alimentação', value: 1 },
+    { label: 'Transporte', value: 2 }
+  ];
+
+  pessoas = [
+    { label: 'Sônia L. Cavalcanti', value: 1 },
+    { label: 'Antônio J. Filho', value: 2 },
+    { label: 'Débora N. Santos', value: 3  },
+  ];
+
 }
