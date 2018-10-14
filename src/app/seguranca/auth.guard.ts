f4dc26e113b6558041f8c4a4169fa18f69f37260
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-10-14 20:45:16 -0300

    Tratando erro de usuários deslogados tentando acessar URL's (M19a15)

--- src/app/seguranca/auth.guard.ts
+++ src/app/seguranca/auth.guard.ts
@@ -13,6 +13,19 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+    if (this.oauth.tokenExpirado()) {
+      return this.oauth.obterNovoAccessToken()
+        .then(() => {
+          if (this.oauth.tokenExpirado()) {
+            this.router.navigate(['/login']);
+            return false;
+          }
+
+          return true;
+        });
+    }
+
     if (next.data.roles && !this.oauth.temPermissaoRequerida(next.data.roles)) {
       this.router.navigate(['/403']);
       return false;
