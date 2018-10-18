fa440cdbeb2ce3288b73e612e6e46f979aa1de99
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-10-18 19:19:12 -0300

    Correção 2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('/dist'));
 
 app.get('/*', function(req, res){
-  res.sendFile(__dirname +'/dist/index.html');
+  res.sendFile('/dist/index.html');
 });
 
 app.listen(process.env.PORT ||4200);
