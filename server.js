12d675449752839219600f4dc5fefae60c0e51b5
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-10-18 19:54:39 -0300

    Deploy 4

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/easymanager-frontend'));
 
 app.get('/*', function(req, res){
-  res.header("Access-Control-Allow-Origin", req.url);
-  res.header('Access-Control-Allow-Methods', 'PUT, PATCH, GET, POST, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Access-Control-Allow-Credentials', true);
   res.sendFile(__dirname +'/dist/easymanager-frontend/index.html');
 });
 
