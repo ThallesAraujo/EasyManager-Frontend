565ac0d2dcc27d6e4466bcaa872dc0eeef0841b7
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-10-18 19:27:11 -0300

    Deploy 3

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('/dist'));
+app.use(express.static(__dirname + '/dist/easymanager-frontend'));
 
 app.get('/*', function(req, res){
-  res.sendFile('/dist/index.html');
+  res.sendFile(__dirname +'/dist/easymanager-frontend/index.html');
 });
 
-app.listen(process.env.PORT ||4200);
+app.listen(process.env.PORT||4200);
